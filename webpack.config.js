6cbb40213dee58217de2f9722fc3142682b881a7
jessezisme <13631947+jessezisme@users.noreply.github.com>
2022-03-10 09:54:33 -0500

    Issue 21: remove lodash and jquery front-end dependencies (#22)
    * Styling cleanup (#18)
    
    * styling cleanup: remove floats.
    
    * Issue 21: remove jquery and lodash dependencies.
    
    * add google analytics; shorten animation duration; number styling cleanup; move favicon to assets folder.
    
    * Change html file to handlebars to handle environment differences; Add netlify.toml file for deployments.
    
    * Add deploy-preview context to netlify.toml
    
    * update netlify.toml

--- webpack.config.js
+++ webpack.config.js
@@ -3,90 +3,107 @@ const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const HandlebarsPlugin = require('handlebars-webpack-plugin');
 
 module.exports = (ENV, ARGV) => {
-  return {
-    entry: ['./src/js/main.js'],
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'js/all.js',
-    },
-    watchOptions: {
-      ignored: /node_modules/,
-      aggregateTimeout: 1000,
-    },
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
-      },
-      devMiddleware: {
-        writeToDisk: true,
-      },
-      watchFiles: {
-        paths: ['src/*.html', 'src/**/*.html', 'dist/**/*', 'dist/*'],
-        options: {
-          usePolling: true,
+    const isNodeEnvDefined = ENV.NODE_ENV === 'production' || ENV.NODE_ENV === 'development';
+
+    // Check that webpack ENV.NODE_ENV variable has been set to valid value, i.e. 'production' or 'development'
+    if (!isNodeEnvDefined) {
+        throw new Error(
+            `Webpack --env NODE_ENV variable must be set to development or production, 
+            i.e. --env NODE_ENV=development or --env NODE_ENV=production`
+        );
+    }
+
+    return {
+        entry: ['./src/js/main.js'],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/all.js',
+            clean: true,
         },
-      },
-      hot: false,
-      compress: false,
-      port: 4000,
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [
-          { from: 'src/index.html', to: '' },
-          { from: 'src/favicon', to: 'favicon' },
-        ],
-      }),
-      new MiniCssExtractPlugin({
-        filename: '/css/main.css',
-      }),
-      new webpack.ProvidePlugin({
-        jQuery: 'jquery',
-        jquery: 'jquery',
-        $: 'jquery',
-        Hammer: 'hammerjs',
-        _: 'lodash',
-      }),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
+        watchOptions: {
+            ignored: /node_modules/,
+            aggregateTimeout: 1000,
         },
-        {
-          test: /\.s[ac]ss$/i,
-          include: [path.resolve(__dirname, 'src/style')],
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'dist'),
             },
-            {
-              loader: 'css-loader',
+            devMiddleware: {
+                writeToDisk: true,
             },
-            {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  sourceMap: true,
-                  plugins: ['autoprefixer'],
+            watchFiles: {
+                paths: ['src/*.html', 'src/**/*.html', 'dist/**/*', 'dist/*'],
+                options: {
+                    usePolling: true,
                 },
-              },
             },
-            {
-              loader: 'sass-loader',
-            },
-          ],
+            hot: false,
+            compress: false,
+            port: 4000,
         },
-      ],
-    },
-  };
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    // { from: 'src/index.html', to: '' },
+                    { from: 'src/assets', to: 'assets' },
+                ],
+            }),
+            new HandlebarsPlugin({
+                entry: path.join(__dirname, 'src', '*.hbs'),
+                output: path.join(__dirname, 'dist', '[name].html'),
+                data: {
+                    ENV: ENV,
+                    isEnvDev: ENV.NODE_ENV === 'development',
+                    isEnvProd: ENV.NODE_ENV === 'production',
+                },
+            }),
+            new MiniCssExtractPlugin({
+                filename: '/css/main.css',
+            }),
+            new webpack.ProvidePlugin({
+                Hammer: 'hammerjs',
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    include: [path.resolve(__dirname, 'src/style')],
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                postcssOptions: {
+                                    sourceMap: true,
+                                    plugins: ['autoprefixer'],
+                                },
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                        },
+                    ],
+                },
+            ],
+        },
+    };
 };
