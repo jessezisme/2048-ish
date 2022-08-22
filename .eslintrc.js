6f72b1e916d6be14b0ea2b51f28a5a0f4c206d08
jessezisme <13631947+jessezisme@users.noreply.github.com>
2022-08-22 08:38:53 -0400

    Closes #21 - removes Lodash and jQuery dependencies (#25)
    - Issue #21: remove lodash and jquery front-end dependencies (#22)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jquery: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'prefer-const': ['error'],
-    'no-unused-vars': ['warn']
-  },
-  globals: {
-    '_': 'readonly', 
-    'Hammer': 'readonly', 
-    '$': 'readonly', 
-  }
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'prefer-const': 'error',
+        'no-unused-vars': 'warn',
+    },
+    globals: {
+        Hammer: 'readonly',
+    },
 };
